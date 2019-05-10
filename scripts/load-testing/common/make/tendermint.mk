340d00c6caa17f1592b90079836079541afa54ab
Thane Thomson <connect@thanethomson.com>
2019-05-10 12:00:11 -0400

    Remove scripts folder to make way for better approach

--- scripts/load-testing/common/make/tendermint.mk
+++
@@ -1,8 +0,0 @@
-GOPATH?=$(shell go env GOPATH)
-TMSRC=$(GOPATH)/src/github.com/tendermint/tendermint
-TMBIN=$(TMSRC)/build/tendermint
-
-$(TMBIN):
-	@echo "Building Tendermint Linux executable..."
-	$(MAKE) -C $(TMSRC) build-linux
-
