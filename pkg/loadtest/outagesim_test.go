cc0c68d8f49a643673544f87a40cc88c949046f6
Thane Thomson <connect@thanethomson.com>
2019-05-12 12:31:31 -0400

    Expand test cases for outage simulator

--- pkg/loadtest/outagesim_test.go
+++ pkg/loadtest/outagesim_test.go
@@ -1,6 +1,7 @@
 package loadtest_test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -15,50 +16,60 @@ const (
 	testOutageSimPassword = "testpassword"
 )
 
-type testOutageResult struct {
+type outageSimTestCase struct {
+	plan                  string
+	user                  string
+	password              string
+	expectSyntaxError     bool
+	expectRequestFailures bool
+	expectedNode1Results  []outageSimTestResult
+	expectedNode2Results  []outageSimTestResult
+}
+
+type outageSimTestResult struct {
 	code       int
 	nodeStatus loadtest.NodeStatus
 }
 
-func makeOutageSimHandler(resultc chan testOutageResult) func(w http.ResponseWriter, r *http.Request) {
+func makeOutageSimHandler(resultc chan outageSimTestResult) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		u, p, _ := r.BasicAuth()
 		if u != testOutageSimUser || p != testOutageSimPassword {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			resultc <- testOutageResult{code: http.StatusUnauthorized}
+			resultc <- outageSimTestResult{code: http.StatusUnauthorized}
 			return
 		}
 		if r.Method != "POST" {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			resultc <- testOutageResult{code: http.StatusMethodNotAllowed}
+			resultc <- outageSimTestResult{code: http.StatusMethodNotAllowed}
 			return
 		}
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			resultc <- testOutageResult{code: http.StatusInternalServerError}
+			resultc <- outageSimTestResult{code: http.StatusInternalServerError}
 			return
 		}
 		switch string(body) {
 		case string(loadtest.NodeUp), string(loadtest.NodeDown):
 			w.WriteHeader(http.StatusOK)
-			resultc <- testOutageResult{code: http.StatusOK, nodeStatus: loadtest.NodeStatus(string(body))}
+			resultc <- outageSimTestResult{code: http.StatusOK, nodeStatus: loadtest.NodeStatus(string(body))}
 
 		default:
 			http.Error(w, "Invalid request", http.StatusBadRequest)
-			resultc <- testOutageResult{code: http.StatusBadRequest}
+			resultc <- outageSimTestResult{code: http.StatusBadRequest}
 		}
 	}
 }
 
-func TestOutageSimulatorHappyPath(t *testing.T) {
-	ts1resultc := make(chan testOutageResult, 1)
+func TestOutageSimulator(t *testing.T) {
+	ts1resultc := make(chan outageSimTestResult, 1)
 	mux1 := http.NewServeMux()
 	mux1.HandleFunc("/", makeOutageSimHandler(ts1resultc))
 	ts1 := httptest.NewServer(mux1)
 	defer ts1.Close()
 
-	ts2resultc := make(chan testOutageResult, 1)
+	ts2resultc := make(chan outageSimTestResult, 1)
 	mux2 := http.NewServeMux()
 	mux2.HandleFunc("/", makeOutageSimHandler(ts2resultc))
 	ts2 := httptest.NewServer(mux2)
@@ -68,81 +79,151 @@ func TestOutageSimulatorHappyPath(t *testing.T) {
 		"node1": ts1.URL,
 		"node2": ts2.URL,
 	}
-	sim, err := loadtest.NewOutageSimulator(
-		`
-			node1=10ms:down,5ms:up,25ms:down
-			node2=5ms:down,30ms:up
-		`,
-		testOutageSimUser,
-		testOutageSimPassword,
-		nodeURLs,
-	)
-	if err != nil {
-		t.Fatal(err)
+
+	testCases := []outageSimTestCase{
+		// 0
+		outageSimTestCase{
+			plan: `
+				node1=10ms:down,5ms:up,25ms:down
+				node2=5ms:down,30ms:up
+			`,
+			user:     testOutageSimUser,
+			password: testOutageSimPassword,
+			expectedNode1Results: []outageSimTestResult{
+				outageSimTestResult{
+					code:       http.StatusOK,
+					nodeStatus: loadtest.NodeDown,
+				},
+				outageSimTestResult{
+					code:       http.StatusOK,
+					nodeStatus: loadtest.NodeUp,
+				},
+				outageSimTestResult{
+					code:       http.StatusOK,
+					nodeStatus: loadtest.NodeDown,
+				},
+			},
+			expectedNode2Results: []outageSimTestResult{
+				outageSimTestResult{
+					code:       http.StatusOK,
+					nodeStatus: loadtest.NodeDown,
+				},
+				outageSimTestResult{
+					code:       http.StatusOK,
+					nodeStatus: loadtest.NodeUp,
+				},
+			},
+		},
+		// 1
+		outageSimTestCase{
+			plan: `
+				node1=10ms:down,5ms:up
+			`,
+			user:     testOutageSimUser,
+			password: testOutageSimPassword,
+			expectedNode1Results: []outageSimTestResult{
+				outageSimTestResult{
+					code:       http.StatusOK,
+					nodeStatus: loadtest.NodeDown,
+				},
+				outageSimTestResult{
+					code:       http.StatusOK,
+					nodeStatus: loadtest.NodeUp,
+				},
+			},
+			expectedNode2Results: []outageSimTestResult{},
+		},
+		// 2
+		outageSimTestCase{
+			plan: `
+				node1=10ms:down,5ms:up node2=10ms:down,5ms:up
+			`,
+			expectSyntaxError: true,
+		},
+		// 3
+		outageSimTestCase{
+			plan: `
+				node1=node2=10ms:down,5ms:up
+			`,
+			expectSyntaxError: true,
+		},
+		// 4
+		outageSimTestCase{
+			plan: `
+				node1=10ms:down,5ms:up,25ms:down
+				node2=5ms:down,30ms:up
+			`,
+			user:                  testOutageSimUser,
+			password:              "wrongpassword",
+			expectRequestFailures: true,
+			expectedNode1Results: []outageSimTestResult{
+				outageSimTestResult{
+					code: http.StatusUnauthorized,
+				},
+			},
+			expectedNode2Results: []outageSimTestResult{
+				outageSimTestResult{
+					code: http.StatusUnauthorized,
+				},
+			},
+		},
 	}
-	errc := make(chan error, 1)
-	sim.Start(errc)
 
-	node1Results := make([]testOutageResult, 0)
-	node2Results := make([]testOutageResult, 0)
-waitLoop:
-	for {
-		select {
-		case err := <-errc:
-			t.Fatal(err)
-
-		case r := <-ts1resultc:
-			node1Results = append(node1Results, r)
-			if len(node1Results) == 3 && len(node2Results) == 2 {
-				break waitLoop
+testCaseLoop:
+	for i, tc := range testCases {
+		sim, err := loadtest.NewOutageSimulator(
+			tc.plan,
+			tc.user,
+			tc.password,
+			nodeURLs,
+		)
+		if err != nil {
+			if tc.expectSyntaxError {
+				continue testCaseLoop
 			}
-
-		case r := <-ts2resultc:
-			node2Results = append(node2Results, r)
-			if len(node1Results) == 3 && len(node2Results) == 2 {
-				break waitLoop
-			}
-
-		case <-time.After(1 * time.Second):
-			t.Fatal("test timed out")
+			t.Fatalf("test case %d: %v", i, err)
 		}
-	}
-	sim.Wait()
+		errc := make(chan error, 1)
+		sim.Start(errc)
 
-	expectedNode1Results := []testOutageResult{
-		testOutageResult{
-			code:       http.StatusOK,
-			nodeStatus: loadtest.NodeDown,
-		},
-		testOutageResult{
-			code:       http.StatusOK,
-			nodeStatus: loadtest.NodeUp,
-		},
-		testOutageResult{
-			code:       http.StatusOK,
-			nodeStatus: loadtest.NodeDown,
-		},
-	}
-	expectedNode2Results := []testOutageResult{
-		testOutageResult{
-			code:       http.StatusOK,
-			nodeStatus: loadtest.NodeDown,
-		},
-		testOutageResult{
-			code:       http.StatusOK,
-			nodeStatus: loadtest.NodeUp,
-		},
-	}
+		node1Results := make([]outageSimTestResult, 0)
+		node2Results := make([]outageSimTestResult, 0)
+	waitLoop:
+		for {
+			select {
+			case err := <-errc:
+				if !tc.expectRequestFailures {
+					t.Fatalf("test case %d: %v", i, err)
+				}
 
-	if !testOutageResultsEqual(expectedNode1Results, node1Results) {
-		t.Error("got node 1 results:", node1Results, "but expected:", expectedNode1Results)
-	}
-	if !testOutageResultsEqual(expectedNode2Results, node2Results) {
-		t.Error("got node 2 results:", node2Results, "but expected:", expectedNode2Results)
+			case r := <-ts1resultc:
+				node1Results = append(node1Results, r)
+				if len(node1Results) == len(tc.expectedNode1Results) && len(node2Results) == len(tc.expectedNode2Results) {
+					break waitLoop
+				}
+
+			case r := <-ts2resultc:
+				node2Results = append(node2Results, r)
+				if len(node1Results) == len(tc.expectedNode1Results) && len(node2Results) == len(tc.expectedNode2Results) {
+					break waitLoop
+				}
+
+			case <-time.After(1 * time.Second):
+				t.Fatalf("test case %d: test timed out", i)
+			}
+		}
+		sim.Wait()
+
+		if !testOutageResultsEqual(tc.expectedNode1Results, node1Results) {
+			t.Error(fmt.Sprintf("test case %d:", i), "got node 1 results:", node1Results, "but expected:", tc.expectedNode1Results)
+		}
+		if !testOutageResultsEqual(tc.expectedNode2Results, node2Results) {
+			t.Error(fmt.Sprintf("test case %d:", i), "got node 2 results:", node2Results, "but expected:", tc.expectedNode2Results)
+		}
 	}
 }
 
-func testOutageResultsEqual(expected, actual []testOutageResult) bool {
+func testOutageResultsEqual(expected, actual []outageSimTestResult) bool {
 	if len(expected) != len(actual) {
 		return false
 	}
