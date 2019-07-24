21571d8375b4fb65b3658ae2fa8eba4564f18a24
Thane Thomson <connect@thanethomson.com>
2019-07-24 17:06:28 -0400

    Add notes about Prometheus metrics and shutdown-wait parameter to CLI description

--- cmd/tm-load-test/main.go
+++ cmd/tm-load-test/main.go
@@ -4,8 +4,7 @@ import (
 	"github.com/interchainio/tm-load-test/pkg/loadtest"
 )
 
-const appLongDesc = `
-Load testing application for Tendermint with optional master/slave mode.
+const appLongDesc = `Load testing application for Tendermint with optional master/slave mode.
 Generates large quantities of arbitrary transactions and submits those 
 transactions to one or more Tendermint endpoints. By default, it assumes that
 you are running the kvstore ABCI application on your Tendermint network.
@@ -20,6 +19,7 @@ To run the application in MASTER mode:
         master \
         --expect-slaves 2 \
         --bind localhost:26670 \
+        --shutdown-wait 60 \
         -c 1 -T 10 -r 1000 -s 250 \
         --broadcast-tx-method async \
         --endpoints ws://tm-endpoint1.somewhere.com:26657/websocket,ws://tm-endpoint2.somewhere.com:26657/websocket
@@ -27,8 +27,15 @@ To run the application in MASTER mode:
 To run the application in SLAVE mode:
     tm-load-test slave --master localhost:26680
 
-NOTE: In SLAVE mode, all load testing-related flags are ignored. The slave
-always takes instructions from the master node it's connected to.
+NOTES:
+* MASTER mode exposes a "/metrics" endpoint in Prometheus plain text format
+  which shows total number of transactions and the status for the master and
+  all connected slaves.
+* The "--shutdown-wait" flag in MASTER mode is specifically to allow your 
+  monitoring system some time to obtain the final Prometheus metrics from the
+  metrics endpoint.
+* In SLAVE mode, all load testing-related flags are ignored. The slave always 
+  takes instructions from the master node it's connected to.
 `
 
 func main() {
