f501bcbdbb8a805303d1262a437611923d028979
Thane Thomson <connect@thanethomson.com>
2023-01-19 14:19:52 -0500

    Remove unnecessary exit condition check in integration test script
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 make localnet-stop || exit 1
-rm -rf build || exit 1
+rm -rf build
 make localnet-start || exit 1
 go test -count 1 -v --tags=integration -mod=readonly -timeout 8m -coverprofile=coverage.txt -covermode=atomic ./...
 TEST_EXIT_CODE=$?
